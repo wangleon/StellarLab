c2b5c6b78c03d263db08c3e90b945f86648913ce
Liang Wang <wang.leon@gmail.com>
2016-04-27 14:13:47 +0200

    initial git

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name = 'stella',
+    version = '0.1',
+    description = '',
+    author = 'Liang Wang',
+    author_email = 'wang.leon@gmail.com',
+    license = 'BSD',
+    scripts = [
+        #'scripts/findstar',
+        ],
+    packages = [
+        'stella',
+        'stella/catalog',
+        'stella/utils',
+        ],
+    )
