21a86ece8dc2ebf40d961104478d1388e0bed3c5
Liang Wang <wang.leon@gmail.com>
2016-05-18 23:50:09 +0200

    add errors for param

---
+++ stella/param/error.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+class ColorIndexError(Exception):
+    def __init__(self, index, ref):
+        self.index = index
+        self.ref   = ref
+    def __str__(self):
+        return 'Color index %s cannot be used for Teff calibration in %s'%(
+                self.index, self.ref)
+
+class ParamRangeError(Exception):
+    def __init__(self, param, value, ref):
+        self.param = param
+        self.value = value
+        self.ref   = ref
+    def __str__(self):
+        return 'Parameter (%s) = %g is not in the range of %s'%(
+                self.param, self.value, self.ref)
+
+class MissingParamError(Exception):
+    def __init__(self, param, ref):
+        self.param = param
+        self.ref   = ref
+    def __str__(self):
+        return 'Missing input parameter %s in %s'%(self.param, self.ref)
