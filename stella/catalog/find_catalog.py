4fdceb21bfe1d23b2655f1679e2977ef42d0ad62
Liang Wang <wang.leon@gmail.com>
2016-09-07 16:58:14 +0200

    rewrite the query function for TYC2

--- stella/catalog/find_catalog.py
+++ stella/catalog/find_catalog.py
@@ -1,4 +1,5 @@
 import os
+import struct
 import numpy as np
 import astropy.io.fits as fits
 
@@ -65,20 +66,45 @@ def find_TYC(starname):
     if not os.path.exists(filename):
         raise FileNotFond(filename)
 
+    # get tyc1, tyc2, and tyc3
     if starname[0:3]=='TYC':
         g = starname[3:].split('-')
         tyc1 = int(g[0])
         tyc2 = int(g[1])
         tyc3 = int(g[2])
 
+    # get information of FITS table
+    nbyte, nrow, ncol, pos, dtype, fmtfunc = get_bintable_info(filename)
 
-    f = fits.open(filename)
-    data = f[1].data
-    f.close()
-    m1 = data['TYC1']==tyc1
-    m2 = data[m1]['TYC2']==tyc2
-    m3 = data[m1][m2]['TYC3']==tyc3
-    row = data[m1][m2][m3][0]
+    target = np.int32((tyc1<<18) + (tyc2<<4) + (tyc3<<1))
+
+    infile = open(filename)
+    i1, i2 = 0, nrow-1
+    while(i2-i1 > 1):
+        i3 = int((i1+i2)/2)
+        infile.seek(pos + i3*nbyte, 0)
+        key = struct.unpack('>i',infile.read(4))[0]
+        if target < key:
+            i2 = i3
+        elif target > key:
+            i1 = i3
+        else:
+            break
+    infile.seek(-4,1)
+    row = fmtfunc(infile.read(nbyte))
+    
+    # looking for possible companion
+    if i3 != nrow - 1:
+        key2 = struct.unpack('>i',infile.read(4))[0]
+        if key2 == key + 1:
+            infile.seek(-4,1)
+            row2 = fmtfunc(infile.read(nbyte))
+            print 'Warning: There are more than 1 star matched'
+            t1 = (key2 & 0b11111111111111000000000000000000)/2**18
+            t2 = (key2 & 0b00000000000000111111111111110000)/2**4
+            t3 = (key2 & 0b00000000000000000000000000001110)/2
+            print t1,t2,t3,row2
+    infile.close()
     return row
 
 def find_KIC(starname):
