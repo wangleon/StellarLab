82591d2e781bb5ecef0d28d04dafd46817f2d49c
Liang Wang <wang.leon@gmail.com>
2017-12-15 23:50:00 +0100

    adjust the figure layout of color Teff comparision code

--- docs/source/examples/compare_color_Teff/compare_BV_dwarf.py
+++ docs/source/examples/compare_color_Teff/compare_BV_dwarf.py
@@ -8,8 +8,8 @@ def main():
     BVtry_lst = np.arange(0.0,1.8+1e-4,0.01)
     ref_lst = ['Alonso1996','Ramirez2005','GB2009','Casagrande2010']
 
-    fig = plt.figure(figsize=(8,8),dpi=150,tight_layout=True)
-    for ifeh, feh in enumerate([-2,-1,0,0.2]):
+    fig = plt.figure(figsize=(12,4), dpi=150)
+    for ifeh, feh in enumerate([-3,-2,-1,0]):
         BV_lst, teff_lst = {}, {}
         for ref in ref_lst:
             BV_lst[ref] = []
@@ -22,21 +22,29 @@ def main():
                 except:
                     continue
 
-        ax = fig.add_subplot(221+ifeh)
+        ax = fig.add_axes([ifeh*0.23+0.07, 0.15, 0.21, 0.8])
         for ref in ref_lst:
             ax.plot(BV_lst[ref], teff_lst[ref], '-', label=ref)
         leg = ax.legend()
         for t in leg.get_texts():
             t.set_fontsize(8)
         leg.get_frame().set_alpha(0.1)
-        ax.set_xlabel('$B-V$')
-        ax.set_ylabel('$T_{\\rm eff}$')
         x1, x2 = ax.get_xlim()
         y1, y2 = ax.get_ylim()
         ax.text(0.9*x1+0.1*x2, 0.9*y1+0.1*y2, '[Fe/H] = %3.1f'%feh)
+        ax.set_xlabel('$B-V$')
+        for tick in ax.xaxis.get_major_ticks():
+            tick.label1.set_fontsize(10)
+        for tick in ax.yaxis.get_major_ticks():
+            tick.label1.set_fontsize(10)
+        if ifeh == 0:
+            ax.set_ylabel('$T_{\\rm eff}$')
+        else:
+            ax.set_yticklabels([])
         ax.grid(True)
 
-    fig.savefig('compare_BV_dwarf.png')
+    #fig.savefig('compare_BV_dwarf.png')
+    fig.savefig('1.png')
     plt.show()
 
 if __name__=='__main__':
--- docs/source/examples/compare_color_Teff/index.rst
+++ docs/source/examples/compare_color_Teff/index.rst
@@ -6,11 +6,11 @@ Comparison of Color - *T*:sub:`eff` Calibration Relations
 
 (*B* − *V*) v.s. *T*:sub:`eff` Relations for Dwarfs
 ---------------------------------------------------
-.. figure:: compare_BV_dwarf.png
+.. figure:: 1.png
    :alt: B-V vs. Teff for dwarfs
    :align: center
-   :width: 800px
-   :figwidth: 800px
+   :width: 1200px
+   :figwidth: 1200px
 
 
 .. literalinclude:: compare_BV_dwarf.py
