dd769702a7033a442056ddad74c835830044d693
Liang Wang <wang.leon@gmail.com>
2018-02-06 00:24:03 +0100

    remove find_catalog

--- stella/catalog/__init__.py
+++ stella/catalog/__init__.py
@@ -5,4 +5,5 @@ from . import find_catalog
 
 from .hip import HIP
 from .hip2 import HIP2
+from .tyc2 import TYC2
 from . import kepler_planets
--- stella/catalog/find_catalog.py
+++
@@ -1,7 +0,0 @@
-from __future__ import print_function
-import os
-import sys
-import math
-import struct
-import numpy as np
-from .errors import FileNotExist, ItemNotFound, UnrecognizedName
