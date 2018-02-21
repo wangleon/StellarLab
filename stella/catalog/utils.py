0ffd984b3263bd5185ebcfa68ff9564fe0be1f61
Liang Wang <wang.leon@gmail.com>
2018-02-21 23:53:22 +0100

    add HRD histogram plotting function in catalog

--- stella/catalog/utils.py
+++ stella/catalog/utils.py
@@ -7,8 +7,8 @@ def plot_skymap(ra, dec, figfile, projection='hammer', figsize=(8,4.5), dpi=150,
     '''Plot a skymap of sample stars.
 
     Args:
-        ra (list or :class:`numpy.array)`: List of RA.
-        dec (list or :class:`numpy.array)`: List of Dec.
+        ra (list or :class:`numpy.array`): List of RA.
+        dec (list or :class:`numpy.array`): List of Dec.
         figfile (string): Name of output figure.
         projection (string): Projection of map. Avialable options include
             'aitoff', 'mollweide', and 'hammer'.
@@ -84,3 +84,60 @@ def plot_magnitude_histogram(mags, band, figfile, bins, figsize=(8,6), dpi=150,
     fig.savefig(figfile)
     plt.close(fig)
 
+def plot_hrd_histogram(teff, logl, teff_range, logl_range, figfile, figsize=(8,6),
+    dpi=150, scale='log'):
+    '''Plot a 2-D histogram of H-R diagram.
+
+    Args:
+        teff (list or :class:`numpy.array`): List of *T*:sub:`eff`.
+        logl (list or :class:`numpy.array`): List of log\ *L*.
+        teff_range (tuple):
+        logl_range (tuple):
+        figfile (string): Name of output figure.
+        figsize (tuple): Size of figure in tuple (width, height).
+        dpi (integer): DPI of figure.
+    Returns:
+        No returns.
+    '''
+
+    fig = plt.figure(figsize=figsize, dpi=dpi)
+    ax1 = fig.add_axes([0.1,0.1,0.75,0.85])
+    ax2 = fig.add_axes([0.9,0.1,0.02,0.85])
+
+    teff1, teff2, dteff = teff_range
+    logl1, logl2, dlogl = logl_range
+    bins_teff = np.arange(teff1, teff2+1e-6, dteff)
+    bins_logl = np.arange(logl1, logl2+1e-6, dlogl)
+    n_teff = bins_teff.size - 1
+    n_logl = bins_logl.size - 1
+    
+    h, yedge, xedge = np.histogram2d(logl, teff, bins=[bins_logl, bins_teff])
+    if scale=='log':
+        #cax = ax1.imshow(np.log10(h), cmap='Blues', interpolation='none', aspect='auto')
+        import matplotlib.colors as mcolors
+        cax = ax1.hist2d(teff, logl, bins=(bins_teff, bins_logl), cmap='Blues',
+                norm=mcolors.LogNorm())
+        cbar = fig.colorbar(cax[3], cax=ax2)
+        fig.savefig(figfile)
+    else:
+        cax = ax1.imshow(h, cmap='Blues', interpolation='none', aspect='auto')
+    #cbar = fig.colorbar(cax, cax=ax2)
+
+    xticks = np.arange(0, n_teff, int(1000/dteff))
+    yticks = np.arange(0, n_logl, int(1/dlogl))
+    ax1.set_xticks(xticks)
+    ax1.set_yticks(yticks)
+    ax1.set_xticklabels(np.int32(xedge[xticks]))
+    ax1.set_yticklabels(['%3.1f'%yedge[t] for t in yticks])
+    ax1.set_xlim(n_teff-0.5, -0.5)
+    ax1.set_ylim(-0.5, n_logl-0.5)
+    ax1.set_xlabel('$T_\mathrm{eff}$ (K)')
+    ax1.set_ylabel('$\log(L/L_\odot)$')
+    if scale=='log':
+        #cbar.set_label('$\log{N}$')
+        pass
+    else:
+        cbar.set_label('$N$')
+
+    #fig.savefig(figfile)
+    plt.close(fig)
