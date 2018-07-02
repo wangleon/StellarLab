eb0b015fbfda5d36af9c443772c1d22dcb24a064
Liang Wang <wang.leon@gmail.com>
2018-07-02 23:10:05 +0200

    update HIP and TYC plot functions

--- docs/source/examples/catalog_HIP/plot.py
+++ docs/source/examples/catalog_HIP/plot.py
@@ -3,12 +3,10 @@ import os
 import math
 import numpy as np
 import astropy.io.fits as fits
-import matplotlib.pyplot as plt
-import matplotlib.colors as mcolors
 
 from stella.parameter.teff import _BV_to_Teff_Flower1996
 from stella.parameter.bc import _Teff_to_BC_Flower1996
-from stella.catalog.utils import plot_skymap
+from stella.catalog.utils import plot_skymap, plot_histogram, plot_histogram2d
 
 def main():
 
@@ -26,26 +24,13 @@ def main():
     # plot magnitude histogram
     mask = np.isnan(data['Vmag'])
     vmag = data[~mask]['Vmag']
-    bins = np.arange(-2,16)
-    fig = plt.figure(figsize=(8,6), dpi=150)
-    ax = fig.add_axes([0.1,0.1,0.88,0.85])
-    ax.hist(vmag, bins=bins, color='#1166aa', rwidth=0.9)
-    ax.set_axisbelow(True)
-    ax.set_facecolor('#dddddd')
-    ax.yaxis.grid(True, color='w', linestyle='-', linewidth=1)
-    ax.set_yscale('log')
-    # change tick size
-    for tick in ax.xaxis.get_major_ticks():
-        tick.label1.set_fontsize(13)
-    for tick in ax.yaxis.get_major_ticks():
-        tick.label1.set_fontsize(13)
-    ax.set_xlabel('$V$', fontsize=15)
-    ax.set_ylabel('$N$', fontsize=15)
-    ax.set_xticks(np.arange(-2, 16+1e-3, 2))
-    ax.set_xlim(-3, 16)
-    # save the figure
-    fig.savefig('maghist_hip.png')
-    plt.close(fig)
+
+    plot_histogram(vmag,
+            bins    = np.arange(-2, 16),
+            figfile = 'maghist_hip.png',
+            xlabel  ='$V$',
+            xticks  = np.arange(-2, 17, 2),
+            )
 
     #---------------------------------------------------------------------------
     # plot HRD
@@ -62,52 +47,15 @@ def main():
     Mbol = Mv[mask] + BC
     logL = 0.4*(4.74-Mbol)
 
-    #---------------------------------------------------------------------------
-    fig = plt.figure(figsize=(8,6), dpi=150)
-    ax1 = fig.add_axes([0.1,0.1,0.75,0.85])
-    ax2 = fig.add_axes([0.88,0.1,0.03,0.85])
-
-    x1, x2, dx = 3000, 12000, 100
-    y1, y2, dy = -2, 4, 0.1
-    xbins = np.arange(x1, x2+1e-6, dx)
-    ybins = np.arange(y1, y2+1e-6, dy)
-    nx = xbins - 1
-    ny = ybins - 1
-    
-    norm = mcolors.LogNorm()
-
-    _,_,_,cax = ax1.hist2d(Teff, logL, bins=(xbins, ybins), cmap='Blues', norm=norm)
-    cbar = fig.colorbar(cax, cax=ax2)
-
-    # reverse x axis
-    _x1, _x2 = ax1.get_xlim()
-    ax1.set_xlim(_x2, _x1)
-
-    ax1.set_xlabel('$T_\mathrm{eff}$ (K)', fontsize=15)
-    ax1.set_ylabel('$\log(L/L_\odot)$', fontsize=15)
-    cbar.set_label('$N$', fontsize=15)
-
-    c1, c2 = cbar.get_clim()
-    mticks = []
-    power1 = math.floor(math.log10(c1))
-    power2 = math.ceil(math.log10(c2))
-    for power in np.arange(power1, power2):
-        for v in np.arange(1,10)*10**power:
-            if c1 <= v <= c2:
-                mticks.append(v)
-    mticks = cax.norm(mticks)
-    cbar.ax.yaxis.set_ticks(mticks, minor=True)
-
-    # change tick size
-    for tick in ax1.xaxis.get_major_ticks():
-        tick.label1.set_fontsize(13)
-    for tick in ax1.yaxis.get_major_ticks():
-        tick.label1.set_fontsize(13)
-    cbar.ax.tick_params(labelsize=13)
-
-    fig.savefig('hrdhist_hip.png')
-    plt.close(fig)
-
+    plot_histogram2d(Teff, logL,
+            xbins     = np.arange(3000, 12001, 100),
+            ybins     = np.arange(-2, 4.01, 0.1),
+            xlabel    = '$T_\mathrm{eff}$ (K)',
+            ylabel    = '$\log(L/L_\odot)$',
+            figfile   = 'hrdhist_hip.png',
+            reverse_x = True,
+            scale     = 'log',
+            )
 
 if __name__=='__main__':
     main()
--- docs/source/examples/catalog_TYC/plot.py
+++ docs/source/examples/catalog_TYC/plot.py
@@ -2,35 +2,10 @@
 import os
 import numpy as np
 import astropy.io.fits as fits
-import matplotlib.pyplot as plt
 
 from stella.parameter.teff import _BV_to_Teff_Flower1996
 from stella.parameter.bc import _Teff_to_BC_Flower1996
-from stella.catalog.utils import plot_skymap
-
-def plot_histogram(vtmag, figfile):
-    # plot magnitude histogram
-    bins = np.arange(-2,16)
-    fig = plt.figure(figsize=(8,6), dpi=150)
-    ax = fig.add_axes([0.1,0.1,0.88,0.85])
-    ax.hist(vtmag, bins=bins, color='#1166aa', rwidth=0.9)
-    ax.set_axisbelow(True)
-    ax.set_facecolor('#dddddd')
-    ax.yaxis.grid(True, color='w', linestyle='-', linewidth=1)
-    ax.set_yscale('log')
-    # change tick size
-    for tick in ax.xaxis.get_major_ticks():
-        tick.label1.set_fontsize(13)
-    for tick in ax.yaxis.get_major_ticks():
-        tick.label1.set_fontsize(13)
-    ax.set_xlabel('$V_\mathrm{T}$', fontsize=15)
-    ax.set_ylabel('$N$', fontsize=15)
-    ax.set_xticks(np.arange(-2, 16+1e-3, 2))
-    ax.set_xlim(-2, 16)
-    ax.set_ylim(0.5, 2e6)
-    # save the figure
-    fig.savefig(figfile)
-    plt.close(fig)
+from stella.catalog.utils import plot_skymap, plot_histogram, plot_histogram2d
 
 def main():
 
@@ -46,7 +21,12 @@ def main():
     # plot magnitude histogram
     mask = np.isnan(data['VTmag'])
     vtmag = data[~mask]['VTmag']
-    plot_histogram(vtmag, 'maghist_tyc2.png')
+    plot_histogram(vtmag,
+            bins    = np.arange(-2,16),
+            figfile = 'maghist_tyc2.png',
+            xlabel  = '$V_\mathrm{T}$',
+            ylim    = (0.5, 2e6),
+            xticks  = np.arange(-2, 17, 2))
 
     #---------------------------------------------------------------------------
     # read data file
@@ -61,7 +41,12 @@ def main():
     # plot magnitude histogram
     mask = np.isnan(data['VTmag'])
     vtmag = data[~mask]['VTmag']
-    plot_histogram(vtmag, 'maghist_tyc.png')
+    plot_histogram(vtmag,
+            bins    = np.arange(-2,16),
+            figfile = 'maghist_tyc.png',
+            xlabel  = '$V_\mathrm{T}$',
+            ylim    = (0.5, 2e6),
+            xticks  = np.arange(-2, 17, 2))
 
 if __name__=='__main__':
     main()
