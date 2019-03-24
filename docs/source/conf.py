f8ac997a0369bee882c4ab38ea8e2090e791a1a7
Liang Wang <wang.leon@gmail.com>
2019-03-24 21:38:39 +0100

    comment all extensions about matplotlib

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ import sys, os
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
     #'matplotlib.sphinxext.only_directives',
-    'matplotlib.sphinxext.plot_directive',
+    #'matplotlib.sphinxext.plot_directive',
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.mathjax',
